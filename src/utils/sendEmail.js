1aeb014977a35a9477389ce6555c2403cf0e7f5e
begumnarmanli <begumnarmanli@gmail.com>
2025-11-09 13:54:57 +0300

    fix: Nodemailer 465 portu için secure: true ayarı son hali

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -7,7 +7,7 @@ export const sendEmail = async (data) => {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: false,
+    secure: true,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASSWORD,
